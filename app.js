78862a578eb518f235c8498c0f3b1f52b6d4c287
ehtisham57 <81214666+ehtisham57@users.noreply.github.com>
2021-12-13 21:57:26 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+///Question 1
+
+// var a = {
+//     name:["juice","cookie","shirt","pen"],
+//    price:[50,30,880,100],
+//    quantity:[40,9,1,2],
+//     total : function(){
+//         return ;
+//     }
+// }
+   
+
+
+// console.log(a.total())
+// document.write(a.total())
+
+
+//Question 2
+
+var a ={
+    Fname:"Ehtisham",
+    Lname: "Balkhi",
+    age:20,
+    gender:"Male",
+    city:"Karachi",
+    country:"Pakistan"
+}
